db060e28ae33ba0217034034f677d808bc78af6f
yvahosc <yvahosc@unal.edu.co>
2022-04-09 17:42:39 -0500

    Creación de la clase nave no tripulada
    Creación de la clase nave no tripulada la cual extiende de la clase abstracta e implementa la interface con sus atributos propios y sus métodos

---
+++ src/Classes/unmannedSpacecraft.java
@@ -0,0 +1,57 @@
+package Classes;
+
+import Interfaces.ISpacecraft;
+
+public class unmannedSpacecraft extends Spacecraft implements ISpacecraft {
+    private float push;
+
+    public unmannedSpacecraft(
+            String name,
+            String originCountry,
+            float weight,
+            int yearOfCreation,
+            String fuelType,
+            float push)
+    {
+        super(name, originCountry, weight, yearOfCreation, fuelType);
+        this.push = push;
+    }
+
+    public float getPush() {
+        return push;
+    }
+
+    public void setPush(float push) {
+        this.push = push;
+    }
+
+    @Override
+    public void function() {
+        System.out.println("La función principal de un vehículo no tripulado " +
+                "es estudiar otros cuerpos celestes." + "\n");
+    }
+
+    @Override
+    public void boardSpacecraft() {
+        System.out.println("Vehículo aereo no tripulado, no se requiere " +
+                "abordaje.");
+    }
+
+    @Override
+    public void refuelingSpacecraft() {
+        super.setFilledUpTank(true);
+        System.out.println("La nave no tripulada ha sido cargada con " +
+                "combustible.");
+    }
+
+    @Override
+    public void turnOnSpaceCraft() {
+        if (super.isFilledUpTank() == false){
+            System.out.println("Es necesario poner combustible a la nave no " +
+                    "tripulada antes de encenderla.");
+        } else{
+            super.setSpacecraftTurnedOn(true);
+            System.out.println("Nave no tripulada encendida.");
+        }
+    }
+}
