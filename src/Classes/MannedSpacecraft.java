72b6caf65789a811bb9cbec3d2efb9509a4ca49d
yvahosc <yvahosc@unal.edu.co>
2022-04-10 15:55:39 -0500

    Creación de la clase nave tripulada

---
+++ src/Classes/MannedSpacecraft.java
@@ -0,0 +1,64 @@
+package Classes;
+
+import Interfaces.ISpacecraft;
+
+public class MannedSpacecraft extends Spacecraft implements ISpacecraft {
+    public int capacity;
+
+    public MannedSpacecraft(
+            String name,
+            String originCountry,
+            float weight,
+            int yearOfCreation,
+            String fuelType,
+            int capacity)
+    {
+        super(name, originCountry, weight, yearOfCreation, fuelType);
+        this.capacity = capacity;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    @Override
+    public void function() {
+        System.out.println("La función principal de un vehículo tripulado es " +
+                "enviar seres humanos al espacio para tareas de reparación, " +
+                "mantenimiento o investigación, en misiones donde se precisa " +
+                "de la destreza y de la toma de decisiones de personas, en  " +
+                "detrimento de las máquinas."+ "\n");
+    }
+
+    @Override
+    public void boardSpacecraft() throws InterruptedException {
+        System.out.println("Tripulación abordando...");
+        for (int i = 0;i < this.capacity; i++){
+            Thread.sleep(1000);
+            System.out.println("Abordando tripulante " + (i + 1) + " ...");
+        }
+        System.out.println("Abordaje exitoso.");
+    }
+
+    @Override
+    public void refuelingSpacecraft() {
+        super.setFilledUpTank(true);
+        System.out.println("La nave tripulada ha sido cargada con combustible" +
+                ".");
+    }
+
+    @Override
+    public void turnOnSpaceCraft() {
+        if (super.isFilledUpTank() == false){
+            System.out.println("Es necesario poner combustible a la nave " +
+                    "tripulada antes de encenderla.");
+        } else{
+            super.setSpacecraftTurnedOn(true);
+            System.out.println("Nave tripulada encendida.");
+        }
+    }
+}
