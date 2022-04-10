9f66b4af8ca449e9f3c4ca20b72d6ea898a377eb
yvahosc <yvahosc@unal.edu.co>
2022-04-10 18:48:34 -0500

    Documentando la interface nave espacial

--- src/Interfaces/ISpacecraft.java
+++ src/Interfaces/ISpacecraft.java
@@ -1,8 +1,20 @@
 package Interfaces;
 
+/**
+ * Representa la interface nave espacial y sus métodos.
+ * @author Yeisson Vahos Cortes
+ */
 public interface ISpacecraft {
+
+    /**
+     * Representa la declaración del método para cargar combustibles a las
+     * naves espaciales.
+     */
     public void refuelingSpacecraft();
 
+    /**
+     * Representa la declaración del método para encender las naves espaciales. 
+     */
     public void turnOnSpacecraft();
 
 }
