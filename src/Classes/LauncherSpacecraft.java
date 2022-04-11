3e4c3de12f0e4c05d2f0dddc2fdda92100f2ed6a
yvahosc <yvahosc@unal.edu.co>
2022-04-10 22:02:30 -0500

    Documentando la clase nave lanzadera

--- src/Classes/LauncherSpacecraft.java
+++ src/Classes/LauncherSpacecraft.java
@@ -2,11 +2,38 @@ package Classes;
 
 import Interfaces.ISpacecraft;
 
+/**
+ * Representa la clase nave espacial lanzadora que extiende de la clase
+ * abstracta nave espacial e implementa la interface nave espacial, sus
+ * atributos y sus métodos.
+ * @author Yeisson Vahos Cortes
+ */
 public class LauncherSpacecraft extends Spacecraft implements ISpacecraft {
 
+    /**
+     * Representa el peso que tiene la capacidad de transportar la nave
+     * espacial.
+     */
     private int capacityTransport;
+
+    /**
+     * Representa el empuje que tiene la nave espacial.
+     */
     private int push;
 
+    /**
+     * Representa el constructor que crea una instancia de la clase nave
+     * espacial lanzadora.
+     * @param name Representa el nombre de la nave espacial.
+     * @param originCountry Representa el país de origen de la nave espacial.
+     * @param weight Representa el peso de la nave espacial.
+     * @param yearOfCreation Representa el año de creación de la nave espacial.
+     * @param fuelType Representa el tipo de combustible utilizado por la
+     *                 nave espacial.
+     * @param capacityTransport Representa el peso que tiene la capacidad de
+     *                          transportar la nave espacial.
+     * @param push Representa el empuje que tiene la nave espacial.
+     */
     public LauncherSpacecraft(
             String name,
             String originCountry,
@@ -21,22 +48,48 @@ public class LauncherSpacecraft extends Spacecraft implements ISpacecraft {
         this.push = push;
     }
 
+    /**
+     * Representa el metodo para obtener el peso que tiene la capacidad de
+     * transportar la nave espacial.
+     * @return El valor correspondiente al peso que tiene la capacidad de
+     * transportar la nave espacial.
+     */
     public int getCapacityTransport() {
         return capacityTransport;
     }
 
+    /**
+     * Representa el metodo para modificar el peso que tiene la capacidad de
+     * transportar la nave espacial.
+     * @param capacityTransport El valor que se quiere asignar al atributo
+     *                          correspondiente al peso que tiene la
+     *                          capacidad de transportar la nave espacial.
+     */
     public void setCapacityTransport(int capacityTransport) {
         this.capacityTransport = capacityTransport;
     }
 
+    /**
+     * Representa el metodo para obtener el empuje que tiene la nave espacial.
+     * @return El valor correspondiente al empuje que tiene la nave espacial.
+     */
     public int getPush() {
         return push;
     }
 
+    /**
+     * Representa el metodo para modificar el empuje que tiene la nave espacial.
+     * @param push El valor que se quiere asignar al atributo correspondiente
+     *            al empuje que tiene la nave espacial.
+     */
     public void setPush(int push) {
         this.push = push;
     }
 
+    /**
+     * Representa la implementación del método abstracto para conocer la
+     * función de la nave espacial.
+     */
     @Override
     public void function() {
         System.out.println("La función de un vehículo lanzadera es lanzar una" +
@@ -44,12 +97,20 @@ public class LauncherSpacecraft extends Spacecraft implements ISpacecraft {
                 "una sonda o una nave tripulada." + "\n");
     }
 
+    /**
+     * Representa la implementación del método abstracto para abordar la nave
+     * espacial.
+     */
     @Override
     public void boardSpacecraft() {
         System.out.println("Vehículo aereo no tripulado, no se requiere " +
                 "abordaje.");
     }
 
+    /**
+     * Representa la implementación del método abstracto para cargar
+     * combustible a la nave espacial.
+     */
     @Override
     public void refuelingSpacecraft() {
         super.setFilledUpTank(true);
@@ -57,6 +118,10 @@ public class LauncherSpacecraft extends Spacecraft implements ISpacecraft {
                 ".");
     }
 
+    /**
+     * Representa la implementación del método abstracto para encender la
+     * nave espacial.
+     */
     @Override
     public void turnOnSpacecraft() {
         if (super.isFilledUpTank() == false){
@@ -68,10 +133,22 @@ public class LauncherSpacecraft extends Spacecraft implements ISpacecraft {
         }
     }
 
+    /**
+     * Representa método realizar el despegue de la nave lanzadora junto a la
+     * nave de la misión.
+     * @param boardSpaceShip Parámetro que indica si ya se realizó o no el
+     *                       abordaje de pasajeros para proceder con el
+     *                       despegue de la nave.
+     * @return Un valor booleano que indica si se hizo o no el despegue de la
+     * nave lanzadora dependiendo de algunos parámetros correspondientes al
+     * estado de la nave (el cargue del combustible, abordaje y encendido de
+     * la nave).
+     * @throws InterruptedException Control de posible excepción por el uso
+     * del método thread.sleep 
+     */
     public boolean startSpaceShip(boolean boardSpaceShip)
             throws InterruptedException
     {
-
         if (super.isFilledUpTank() == true){
 
             if (boardSpaceShip == true){
