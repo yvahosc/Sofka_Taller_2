a3c4aecf8586d21c2f3574c3ac0a7a5c3c837310
yvahosc <yvahosc@unal.edu.co>
2022-04-10 21:46:38 -0500

    Documentando la clase nave tripulada

--- src/Classes/MannedSpacecraft.java
+++ src/Classes/MannedSpacecraft.java
@@ -2,10 +2,38 @@ package Classes;
 
 import Interfaces.ISpacecraft;
 
+/**
+ * Representa la clase nave espacial tripulada que extiende de la clase
+ * abstracta nave espacial e implementa la interface nave espacial, sus
+ * atributos y sus métodos.
+ * @author Yeisson Vahos Cortes
+ */
 public class MannedSpacecraft extends Spacecraft implements ISpacecraft {
+    /**
+     * Representa la capacidad de abordaje que tiene la nave espacial.
+     */
     public int capacity;
+    /**
+     * Representa la nave espacial lanzadora que utiliza la nave espacial
+     * para hacer el despegue.
+     */
     public LauncherSpacecraft launcherSpacecraft;
 
+    /**
+     * Representa el constructor que crea una instancia de la clase nave
+     * espacial tripulada.
+     * @param name Representa el nombre de la nave espacial.
+     * @param originCountry Representa el país de origen de la nave espacial.
+     * @param weight Representa el peso de la nave espacial.
+     * @param yearOfCreation Representa el año de creación de la nave espacial.
+     * @param fuelType Representa el tipo de combustible utilizado por la
+     *                 nave espacial.
+     * @param capacity Representa la capacidad de abordaje que tiene la nave
+     *                 espacial.
+     * @param launcherSpacecraft Representa la nave espacial lanzadora que
+     *                           utiliza la nave espacial para hacer el
+     *                           despegue.
+     */
     public MannedSpacecraft(String name,
                             String originCountry,
                             float weight,
@@ -19,22 +47,53 @@ public class MannedSpacecraft extends Spacecraft implements ISpacecraft {
         this.launcherSpacecraft = launcherSpacecraft;
     }
 
+    /**
+     * Representa el metodo para obtener la capacidad de abordaje que tiene
+     * la nave espacial.
+     * @return El valor correspondiente a la capacidad de abordaje que tiene
+     * la nave espacial.
+     */
     public int getCapacity() {
         return capacity;
     }
 
+    /**
+     * Representa el metodo para modificar la capacidad de abordaje que tiene
+     * la nave espacial.
+     * @param capacity El valor que se quiere asignar al atributo
+     *                 correspondiente a la capacidad de abordaje que tiene
+     *                 la nave espacial.
+     */
     public void setCapacity(int capacity) {
         this.capacity = capacity;
     }
 
+    /**
+     * Representa el metodo para obtener la nave espacial lanzadora que
+     * utiliza la nave espacial para hacer el despegue.
+     * @return El valor correspondiente a la nave espacial lanzadora que
+     * utiliza la nave espacial para hacer el despegue.
+     */
     public LauncherSpacecraft getLauncherSpacecraft() {
         return launcherSpacecraft;
     }
 
+    /**
+     * Representa el metodo para modificar la nave espacial lanzadora que
+     * utiliza la nave espacial para hacer el despegue.
+     * @param launcherSpacecraft El valor que se quiere asignar al atributo
+     *                           correspondiente a la nave espacial lanzadora
+     *                           que utiliza la nave espacial para hacer el
+     *                           despegue.
+     */
     public void setLauncherSpacecraft(LauncherSpacecraft launcherSpacecraft) {
         this.launcherSpacecraft = launcherSpacecraft;
     }
 
+    /**
+     * Representa la implementación del método abstracto para conocer la
+     * función de la nave espacial.
+     */
     @Override
     public void function() {
         System.out.println("La función principal de un vehículo tripulado es " +
@@ -44,6 +103,12 @@ public class MannedSpacecraft extends Spacecraft implements ISpacecraft {
                 "detrimento de las máquinas."+ "\n");
     }
 
+    /**
+     * Representa la implementación del método abstracto para abordar la nave
+     * espacial.
+     * @throws InterruptedException Control de posible excepción por el uso
+     * del método thread.sleep
+     */
     @Override
     public void boardSpacecraft() throws InterruptedException {
         System.out.println("Tripulación abordando...");
@@ -54,6 +119,10 @@ public class MannedSpacecraft extends Spacecraft implements ISpacecraft {
         System.out.println("Abordaje exitoso.");
     }
 
+    /**
+     * Representa la implementación del método abstracto para cargar
+     * combustible a la nave espacial.
+     */
     @Override
     public void refuelingSpacecraft() {
         super.setFilledUpTank(true);
@@ -61,6 +130,10 @@ public class MannedSpacecraft extends Spacecraft implements ISpacecraft {
                 ".");
     }
 
+    /**
+     * Representa la implementación del método abstracto para encender la
+     * nave espacial. 
+     */
     @Override
     public void turnOnSpacecraft() {
         if (super.isFilledUpTank() == false){
